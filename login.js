6a6164180d18393432c355f2374c4a20325f6634
jrdemadara <jrdemadara@protonmail.com>
2025-06-30 11:55:49 +0800

    completed login fix

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function login() {
   const email = "";
   const password = "";
-  const device = "";
+  const device = "1234";
   console.log(`Your auth creds is ${email} ${password} on device ${device}`);
 }
